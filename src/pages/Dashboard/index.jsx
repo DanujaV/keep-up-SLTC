e8ad59ce0388d5ada966e023f7922604d9e40998
danu <danujagreru@gmail.com>
2022-08-17 01:55:48 +0530

    changes

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Typography } from '@mui/material';
 
 class Home extends Component {
-    render() { 
+    render() {
         return (
-            <h1>Home Screen</h1>
+            <div>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography>hello</Typography>
+                    </CardContent>
+                </Card>
+            </div>
         );
     }
 }
- 
+
 export default Home;
