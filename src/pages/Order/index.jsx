9477d946adced37f67701f4c2dee7b914849add7
danu <danujagreru@gmail.com>
2022-08-17 01:26:05 +0530

    add routings

---
+++ src/pages/Order/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+    render() { 
+        return (
+            <h1>Order Screen</h1>
+        );
+    }
+}
+ 
+export default Order;
